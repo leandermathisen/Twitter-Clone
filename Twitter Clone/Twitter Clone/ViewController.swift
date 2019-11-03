8e42d3f9e95abdba51817426c669d9d13a6b3d97
Leander Mathisen <leander@Leanders-MBP.hsd1.dc.comcast.net>
2019-11-03 01:53:14 -0500

    started on the ios app UI

---
+++ Twitter Clone/Twitter Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Leander Mathisen on 11/3/19.
+//  Copyright © 2019 Leander Mathisen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
